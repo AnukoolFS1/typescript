141ca282e6bde2287b23e820ec898c20e9ea9a17
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-02-02 19:31:21 +0530

    type any

---
+++ any/any.js
@@ -0,0 +1,10 @@
+// Sometimes you need to store a value in a variable.
+// But you don't know its type at the time writing the program.
+// And the unknown value may come from third party API or user input.
+// In this case, you want to opt-out of the type checking and allow the value to
+// to Pass through the compiler time check
+// To do so, you use the "any" type.
+// The "any" type allows you to assign a value of "any" type to a variable
+const json = `{"latitude":10.11, "longitude": 12.12}`;
+const currentLocation = JSON.parse(json);
+console.log(currentLocation);
