10c5459ce6d7b32070009fb5cbe0506599dcc3d3
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-03-22 22:46:15 +0530

    beginners function in typescript

---
+++ DSA/beginners.js
@@ -0,0 +1,10 @@
+let a = "value";
+let b = "valv";
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+addTwoNumbers(5, 10);
+function Subtraction(a, b) {
+    return a - b;
+}
+console.log(Subtraction(5, 100));
---
+++ DSA/beginners.ts
@@ -0,0 +1,14 @@
+let a = "value"
+let b = "valv"
+function addTwoNumbers(a:number, b:number): number{
+    return a + b
+}
+
+addTwoNumbers(5,10)
+
+function Subtraction(a:number, b:number): number{
+    return a - b
+}
+
+
+console.log(Subtraction(5, 100))
